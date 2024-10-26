45b5c2c2bdd36e56f994e610f290a3821614b346
Beatrizemanuelly <141747437+Beatrizemanuelly@users.noreply.github.com>
2024-10-26 10:57:20 -0300

    s

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ function CriarCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
 
-    cartao.innerHTML = '
-        <div class="cartao_conteudo">
-          <h3>${categoria}</h3>
-        <div class="cartao_conteudo_pergunta">
-              <p>${pergunta}</p>
-       </div>
-       </div> 
-'
+    cartao.innerHTML = `
+        <div class="cartao__conteudo">
+        <h3>${categoria}</h3>
+        <div class="cartao__conteudo__pergunta">
+                <p>${pergunta}</p>
+        </div>
+        
+        <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+        </div>
+        </div>
 
 let respostaEstaVisivel = false 
 function viraCartao() {
